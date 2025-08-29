67e51604ced73ea96b999357ae5f2b16b8fec3a2
unknown <ummayjannatsadia@gmail.com>
2025-08-29 21:46:45 +0600

    all fix

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ for (let i = 0; i < copy.length; i++) {
     copy[i].addEventListener("click", function () {
         totalCopyCount++;
         copyCount.textContent = totalCopyCount;
+
         const hotline = copy[i].parentNode.parentNode.children[1].children[2].innerText;
-        // console.log(hotline)
+
         navigator.clipboard.writeText(hotline)
         alert(`Number is copied`)
     })
@@ -33,27 +34,36 @@ const coin = document.getElementById("coin")
 let totalCoins = 100;
 for (let i = 0; i < calls.length; i++)
     calls[i].addEventListener("click", function () {
+
+        const service = calls[i].parentNode.parentNode.children[1].children[1].innerText;
+        const num = calls[i].parentNode.parentNode.children[1].children[2].innerText;
+
         if (totalCoins >= 20) {
             totalCoins = totalCoins - 20;
             coin.textContent = totalCoins;
+
+            alert(`Calling ${service} ${num}`)
         }
         else {
             alert(`Opps! You're out of coins. you need 20 coins to call.`)
         }
     })
+
 // history add js
 
-for(let call of calls)
-    call.addEventListener("click", function(){
+for (let call of calls) {
+    call.addEventListener("click", function () {
 
-        const callTitle = call.parentNode.parentNode.children[1].children[0];
-        const callNum = call.parentNode.parentNode.children[1].children[2];
+        const callTitle = call.parentNode.parentNode.children[1].children[0].innerText;
+        const callNum = call.parentNode.parentNode.children[1].children[2].innerText;
 
-        const history = document.getElementsByClassName("history")
+        const history = document.getElementsByClassName("history")[0];
         const callHistory = document.createElement("div");
+        const now = new Date()
+        const time = now.toLocaleTimeString();
 
         callHistory.innerHTML = `
-         <div class="flex justify-between p-4 bg-gray-300 my-1">
+         <div class="flex justify-between items-center p-4 bg-gray-100 my-1 rounded-xl">
             <div>
                 <h2>
                     ${callTitle}
@@ -63,24 +73,20 @@ for(let call of calls)
                 </h2>
             </div>
             <div>
+               <h2>
+                   ${time}
+                </h2>
             </div>
         </div>
         `;
 
         history.append(callHistory)
     })
+}
 
-// alert messege 
+// history remove js 
 
-const alertClass = document.getElementsByClassName("call")
-
-for (let alerts of alertClass)
-
-    alerts.addEventListener("click", function () {
-
-        const service = alerts.parentNode.parentNode.children[1].children[1].innerText;
-        const num = alerts.parentNode.parentNode.children[1].children[2].innerText;
-
-        alert(`Calling ${service} ${num}`)
-    }
-    )
+const clear = document.getElementById("clear").addEventListener("click", function () {
+    const history = document.getElementsByClassName("history")[0];
+    history.innerHTML = "";
+})
