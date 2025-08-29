34dffdd8a0131aa9bfc6f8b8c4d9d5dec4d1d444
unknown <ummayjannatsadia@gmail.com>
2025-08-29 20:19:37 +0600

    alert message

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-// console.log(document)
 // heart increase js 
-const heart = document.getElementById("heart")
-const heartCount = document.getElementById("heart-count")
-let totalHeartCount = 0
-heart.addEventListener("click", function(){
+
+const hearts = document.getElementsByClassName("heart"); // HTMLCollection
+const heartCount = document.getElementById("heart-count");
+let totalHeartCount = 0;
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].addEventListener("click", function() {
     totalHeartCount++;
-   heartCount.textContent=  totalHeartCount;
-})
+    heartCount.textContent = totalHeartCount;
+  });}
 
 // copy increase js 
 
-const copy = document.getElementById("copy")
+const copy = document.getElementsByClassName("copy")
 const copyCount = document.getElementById("copy-count")
 let totalCopyCount = 0 
-copy.addEventListener("click", function(){
+for (let i = 0 ; i<copy.length; i++){
+copy[i].addEventListener("click", function(){
     totalCopyCount++;
     copyCount.textContent = totalCopyCount;
 })
+}
 
 //  call coins decrease js 
 
-const call = document.getElementById("call")
+const calls = document.getElementsByClassName("call")
 const coin = document.getElementById("coin")
 let totalCoins = 100;
-call.addEventListener("click", function(){
+for(let i = 0; i<calls.length; i++)
+calls[i].addEventListener("click", function(){
     if(totalCoins>=20){
         totalCoins = totalCoins - 20;
         coin.textContent= totalCoins
@@ -32,3 +36,47 @@ call.addEventListener("click", function(){
         
     }
 })
+// history add js
+
+// for(let call of calls){
+//     call.addEventListener("click", function(){
+
+//         const callTitle = call.parentNode.parentNode.children[1].children[0];
+//         const callNum = call.parentNode.parentNode.children[1].children[2];
+
+//         const history = document.getElementsByClassName("history")
+//         const callHistory = document.createElement("div");
+
+//         callHistory.innerHTML = `
+//          <div class="flex justify-between p-4 bg-gray-300 my-1">
+//             <div>
+//                 <h2>
+//                     ${callTitle}
+//                 </h2>
+//                 <h2 class="text-gray-700">
+//                     ${callNum}
+//                 </h2>
+//             </div>
+//             <div>
+//             </div>
+//         </div>
+//         `;
+
+//         history.append(callHistory)
+//     })
+// }
+
+// alert messege 
+
+const alertClass = document.getElementsByClassName("call")
+
+for(let alerts of alertClass) 
+
+alerts.addEventListener("click", function(){
+
+    const service = alerts.parentNode.parentNode.children[1].children[1].innerText;
+    const num = alerts.parentNode.parentNode.children[1].children[2].innerText;
+    
+    alert(`Calling ${service} ${num}`)
+}
+)
