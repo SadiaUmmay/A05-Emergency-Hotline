7fd690191096ddd2db5e9d3d8610cd659d8fc088
unknown <ummayjannatsadia@gmail.com>
2025-08-29 14:47:07 +0600

    heart and copy increase js

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log(document)
+// heart increase js 
+const heart = document.getElementById("heart")
+const heartCount = document.getElementById("heart-count")
+let totalHeartCount = 0
+heart.addEventListener("click", function(){
+    totalHeartCount++;
+   heartCount.textContent=  totalHeartCount;
+})
+
+// copy increase js 
+
+const copy = document.getElementById("copy")
+const copyCount = document.getElementById("copy-count")
+let totalCopyCount = 0 
+copy.addEventListener("click", function(){
+    totalCopyCount++;
+    copyCount.textContent = totalCopyCount;
+})
+
